cb1f71d25152d49b7bac05e8e1210e80e9b9c0ba
tusharpatil03 <tusha2149@gmail.com>
2025-06-08 17:37:36 +0530

    Setting up Project

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  {
+    // Note: there should be no other properties in this object
+    ignores: [
+      "coverage",
+      "**/public",
+      "**/dist",
+      "pnpm-lock.yaml",
+      "pnpm-workspace.yaml",
+    ],
+  },
+  eslintPluginPrettierRecommended,
+]);
