6473efb033b076236d829d5a294bfbbfc36fd61c
tusharpatil03 <tusha2149@gmail.com>
2025-06-10 23:20:37 +0530

    stopped tracking dist files

--- apps/server/dist/globals.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DATABASE_URL = void 0;
-require("dotenv/config");
-const ENV = process.env;
-exports.DATABASE_URL = ENV.DATABASE_URL;
